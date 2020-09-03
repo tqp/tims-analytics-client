a29c8aabcfa60f55bd4e9547fb85a789e9f72f79
tqp <timothyqpowers@gmail.com>
2020-09-03 15:45:01 -0500

    Moved server back to load balanced https.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   seasonName: 'prod',
   production: true,
   hmr: false,
-  apiUrl: 'http://tims-analytics-server.us-east-1.elasticbeanstalk.com',
+  apiUrl: 'https://server.timsanalytics.com',
   buildTimestamp: '08/12/2020 01:00:02'
 };
